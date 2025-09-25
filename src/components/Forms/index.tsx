9728b020eff4b5875e12549fc42d35eff1a75d2c
Luiz Gustavo <luizgustavobonke@gmail.com>
2025-09-25 11:47:39 -0300

    :sparkles: feat: Validação de dados enviados no campo input e timer para evitar spawn no formulário

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -17,7 +17,7 @@ const styleSelect: CSSProperties = {
     color: '#666',
     textAlign: 'left',
     borderRadius: '7px',
-    fontSize: '14px',
+    fontSize: '22px',
 }
 
 export default function Forms(): ReactElement {
@@ -33,41 +33,76 @@ export default function Forms(): ReactElement {
     })
 
     const [status, setStatus] = useState<string>('');
+    const [errors, setErrors] = useState<string[]>([]);
+    const [isSending, setIsSending] = useState<boolean>(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>): void => {
 
         const target = e.target as EventTarget & { name: string; value: string };
         const { name, value } = target;
-        
+
         setFormData({ ...formData, [name]: value });
+    }
+
+    const validation = (): void => {
+        // Validar campos no input
+        if (!formData) errors.push("Preencha todo o formulário.")
+        if (!/^[A-Za-zÀ-ÿ\s]{2,}$/.test(formData.nome)) errors.push("Por favor, digite um nome válido.");
+        if (!/^[0-9\-\(\)]+$/.test(formData.celular)) errors.push("Numero de celular inválido.")
+        if (!/^[A-Za-zÀ-ÿ\s]{2,}$/.test(formData.marca)) errors.push("Por favor, digite uma marca válida.");
+        if (!formData.eletrodomestico || formData.eletrodomestico === "default") errors.push("Por favor, escolha um eletrodoméstico.")
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) errors.push("Por favor, digite um email válido.");
     }
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
-        const response = await fetch('https://formspree.io/f/xgvywybv', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
+        setErrors([]);
+        validation();
 
-        if (response.ok) {
-            setStatus('Sua solicitação foi enviada. Por Gentileza, aguarde o retorno do técnico.');
-            setFormData({
-                nome: '',
-                email: '',
-                celular: '',
-                endereco: '',
-                eletrodomestico: '',
-                marca: '',
-                defeito: ''
-            })
-        } else {
-            setStatus('Ocorreu algum erro ao enviar a sua solicitação. Tente Novamente!');
+        if (errors.length > 0) {
+            errors.forEach(error => alert(error));
             return;
         }
+
+        if (isSending) {
+            alert("Aguarde antes de enviar novamente.");
+            return;
+        }
+
+        setIsSending(true);
+
+        try {
+            const response = await fetch('https://formspree.io/f/xgvywybv', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                setStatus('Sua solicitação foi enviada. Por Gentileza, aguarde o retorno do técnico.');
+                setFormData({
+                    nome: '',
+                    email: '',
+                    celular: '',
+                    endereco: '',
+                    eletrodomestico: '',
+                    marca: '',
+                    defeito: ''
+                })
+
+
+            } else {
+                setStatus('Ocorreu algum erro ao enviar a sua solicitação. Tente Novamente!');
+                return;
+            }
+        } catch (err) {
+            setStatus("Erro de conexão. Tente novamente.");
+        }
+
+        setTimeout(() => setIsSending(false), 10000);
     }
 
     return (
@@ -89,7 +124,7 @@ export default function Forms(): ReactElement {
                     <input type="text" name="celular" value={formData.celular} onChange={handleChange} placeholder="(11) 91111-2222" required />
                     <input type="text" name="endereco" value={formData.endereco} onChange={handleChange} placeholder="Endereço para visita ( Rua e Cidade )" required />
 
-                    <select name="eletrodomestico" id="" style={styleSelect} onChange={handleChange} value={formData.eletrodomestico} required >
+                    <select name="eletrodomestico" id="eletro" style={styleSelect} onChange={handleChange} value={formData.eletrodomestico} required >
                         <option value="default">Escolha o seu Eletrodoméstico</option>
                         <option value="maq">Máquina de Lavar</option>
                         <option value="gel">Geladeira</option>
@@ -97,12 +132,12 @@ export default function Forms(): ReactElement {
                         <option value="out">Outros</option>
                     </select>
 
-                    <input type="text" name="marca" value={formData.marca} onChange={handleChange}  placeholder="Escolha a Marca" required />
+                    <input type="text" name="marca" value={formData.marca} onChange={handleChange} placeholder="Escolha a Marca" required />
 
-                    <textarea name="defeito" style={{ maxWidth: '100%' }} value={formData.defeito} onChange={handleChange}  placeholder="Descreva o defeito" rows={4} required></textarea>
+                    <textarea name="defeito" style={{ maxWidth: '100%' }} value={formData.defeito} onChange={handleChange} placeholder="Descreva o defeito" rows={4} required></textarea>
                     <button type="submit">Enviar</button>
 
-                    {status && <p>{status}</p>}
+                    {status && isSending && <p>{status}</p>}
                 </form>
             </div>
         </section>
